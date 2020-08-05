74daf38017e703ea62ed7f52b85947f27cc25920
Ari2007 <talyaw@hotmail.com>
2020-08-05 19:04:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#VARIABLE - Something that can store a singlar piece of data
+
+name = "Spongebob"
+score = 0
+height = 62.5
+
+#Boolean data type - piece of data that can turn true or false
+#State variable - similar to Boolean data type
+game_over = True
+
+#Loops - a piece of code that repeats
+
+#for loop - repeats code for a given amount of time (we tell the loop howmany times to repeat the code)
+for i in range(4):
+  print("Hi my name is " + str(name))
+
+#while loop - repeats code until the condition is met (aka turns false)
+
+count = 0
+while count < 3:
+  count += 1
+  print("Beetlejuice", count)
+  
